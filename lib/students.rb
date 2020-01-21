89de4b5cf29a528bada98a5a5a9bc90442334d40
Steven K <stevenks17@gmail.com>
2020-01-21 23:05:45 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -10,10 +10,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+      "SELECT AVG (gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+      "SELECT SUM (tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  
 end
